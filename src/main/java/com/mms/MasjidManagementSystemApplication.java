7c85cff172f438fe166f3d98de6452611ae58138
Khandker Mohammed Nurul Afsar <afsra@therapservices.net>
2018-11-11 04:23:30 +0600

    Added basic Gradle, Spring Boot Configuration/Files

---
+++ src/main/java/com/mms/MasjidManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.mms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasjidManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasjidManagementSystemApplication.class, args);
+	}
+}
